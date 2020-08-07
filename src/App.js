bf346d600be280726070f6ef6eb300277a7e7da3
Nicholas Osborn <nicholas.p.osborn@gmail.com>
2020-08-07 13:40:33 -0400

    playing button + ship placement

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import Board from './Components/gameboard';
+import React, {useState} from 'react';
+import Battleship from './Components/Battleship.js';
 import Console from './Components/Console';
 import Header from './Components/Header';
 import Game from './game/Game.js';
 
 export default function App() {
+    const [playing, setPlaying] = useState(false);
     const setup = () => {
         const game = new Game();
         game.players.forEach(player => {
             game.initializeBoard(player.gameboard);
         })
+        return game;
     }
-    setup();
+    const game = setup();
     // TEMP SETUP
     
 
     return (
         <div className="container mx-auto flex flex-col">
             <Header />
-            <Board />
-            <Console />
+            <Battleship game={game}/>
+            <Console playing={playing} setPlaying={setPlaying}/>
         </div>
     )
 }
